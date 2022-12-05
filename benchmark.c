dee5a34fc1d3e219d29638a34dd180ab11e3d756
Ziv Morgan <67800437+zivm20@users.noreply.github.com>
2022-12-05 18:13:04 +0200

    Create benchmark.c

---
+++ benchmark.c
@@ -0,0 +1,90 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+
+#define PACKET_SIZE 1024
+
+
+int readData(int fd){
+    
+}
+
+int udSocketServer(char* name, size_t dataSize, int socketType){
+    int server, client, len, rc;
+    int bytes_rec = 0;
+    struct sockaddr_un serverAddr;
+    struct sockaddr_un clientAddr;     
+    char buf[PACKET_SIZE];
+    
+    memset(&serverAddr, 0, sizeof(struct sockaddr_un));
+    memset(&clientAddr, 0, sizeof(struct sockaddr_un));
+    memset(buf, 0, PACKET_SIZE);                
+    
+    //create a UNIX domain socket 
+    if ((server = socket(AF_UNIX, socketType, 0)) == -1){
+        printf("SOCKET ERROR: %d\n", sock_errno());
+        exit(1);
+    }
+    
+    //link socket to our file
+    serverAddr.sun_family = AF_UNIX;   
+    strcpy(serverAddr.sun_path, name); 
+    len = sizeof(serverAddr);
+    
+    //make sure it's not linked elsewhere
+    unlink(name);
+    if (bind(server, (struct sockaddr *) &serverAddr, len) == -1){
+        printf("BIND ERROR: %d\n", sock_errno());
+        close(server);
+        exit(1);
+    }
+    
+    //start listening for any client sockets
+    if (listen(server, backlog) == -1){ 
+        printf("LISTEN ERROR: %d\n", sock_errno());
+        close(server);
+        exit(1);
+    }
+    
+    //accept client
+    if ((client = accept(server, (struct sockaddr *) &clientAddr, &len)) == -1){
+        printf("ACCEPT ERROR: %d\n", sock_errno());
+        close(server);
+        close(client);
+        exit(1);
+    }
+    
+    //read data
+    while ((bytes_rec = recv(client, buf, sizeof(buf), 0)) > 0){
+        if(bytes_rec < 0){
+            printf("RECV ERROR: %d\n", sock_errno());
+            close(server);
+            close(client);
+            exit(1);
+        }
+    }
+    else {
+        
+    }
+    
+    
+    
+    //close the sockets and exit 
+    close(server);
+    close(client);
+
+}
+
+
+int main(){
+
+}
+
+
+
